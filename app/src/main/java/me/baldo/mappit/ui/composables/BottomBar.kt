e1a7f4b8287e4f132ae1911474f335f8dcee750f
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2025-06-05 15:45:41 +0200

    Switch to Material 3 Expressive bottom bar

--- app/src/main/java/me/baldo/mappit/ui/composables/BottomBar.kt
+++ app/src/main/java/me/baldo/mappit/ui/composables/BottomBar.kt
@@ -1,12 +1,14 @@
 package me.baldo.mappit.ui.composables
 
+import androidx.compose.foundation.layout.offset
+import androidx.compose.material3.FlexibleBottomAppBar
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import me.baldo.mappit.ui.BottomBarTab
 import me.baldo.mappit.ui.MappItRoute
@@ -16,23 +18,19 @@ fun BottomBar(
     selectedTab: BottomBarTab,
     navController: NavHostController
 ) {
-    NavigationBar {
+    FlexibleBottomAppBar {
         BottomBarTab.tabs.forEach { tab ->
             val selected = selectedTab == tab
             val title = stringResource(id = tab.titleResID)
             NavigationBarItem(
+                modifier = Modifier.offset(y = (2).dp),
                 icon = {
                     Icon(
                         imageVector = if (selected) tab.iconSelected else tab.icon,
                         contentDescription = title
                     )
                 },
-                label = {
-                    Text(
-                        text = title,
-                        style = MaterialTheme.typography.titleSmall
-                    )
-                },
+                label = { Text(text = title) },
                 selected = selected,
                 onClick = {
                     if (tab != selectedTab) {
